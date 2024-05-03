16cdc0621a525076693090db2fa77840cdeb6f4e
karthik30018 <as.karthik30018@gmail.com>
2024-05-03 14:57:29 +0530

    Created React Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './Component/Header';
+import NavBar from './Component/NavBar';
+// import axios from 'axios';
+// import { useState } from'react';
+
 
 function App() {
+//   const [image,setImage]= useState();
+//   axios.get(`https://api.pexels.com/v1/search?query=people`,{
+//   headers: {
+//  'Authorization': 'Wwj9IHFVA4ODJzB7jajv1UBq2QoS3axuNhQVMvX4qq3xmkirMhxoJdgz'
+//   }
+// }).then(res=>{
+//   console.log(res);
+//   setImage(res.data.photos[0].src.original);
+// })
+
+  // const Api_key= "360788d4c20fe976cbac02d7e51a38fc";
+  // Wwj9IHFVA4ODJzB7jajv1UBq2QoS3axuNhQVMvX4qq3xmkirMhxoJdgz
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   
+   
+      <>
+
+      <Header/>
+      </>
+  
+     
   );
 }
 
---
+++ src/Component/NavBar.js
@@ -0,0 +1,8 @@
+function NavBar(){
+  return(
+    <div>
+        <h1>NavBar</h1>
+    </div>
+  )
+}
+export default NavBar;
