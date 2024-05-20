9d6ea2bba211848c0e685d03ffdab348e25dc26a
Karthik A S <143400284+karthik30018@users.noreply.github.com>
2024-05-20 22:16:14 +0530

    Fetching Data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,22 @@
-import './App.css';
-import Header from './Component/Header';
-import NavBar from './Component/NavBar';
-// import axios from 'axios';
-// import { useState } from'react';
+import axios from 'axios'
+
+function App(){
 
 
-function App() {
-//   const [image,setImage]= useState();
-//   axios.get(`https://api.pexels.com/v1/search?query=people`,{
-//   headers: {
-//  'Authorization': 'Wwj9IHFVA4ODJzB7jajv1UBq2QoS3axuNhQVMvX4qq3xmkirMhxoJdgz'
-//   }
-// }).then(res=>{
-//   console.log(res);
-//   setImage(res.data.photos[0].src.original);
-// })
+  axios.get(`http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=360788d4c20fe976cbac02d7e51a38fc`).then((res)=>{
+    console.log(res)
+    
+  }).catch((err)=>{
+    console.error(err)
+  })
+  return(
 
-  // const Api_key= "360788d4c20fe976cbac02d7e51a38fc";
-  // Wwj9IHFVA4ODJzB7jajv1UBq2QoS3axuNhQVMvX4qq3xmkirMhxoJdgz
-
-  return (
-   
-   
-      <>
-
-      <Header/>
-      </>
-  
+    <div>
      
-  );
+
+
+    </div>
+  )
 }
 
 export default App;
